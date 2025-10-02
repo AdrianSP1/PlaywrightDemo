e5aebd5caf74d2f58dd8aec1455be794c100a4c4
Adrian <adriangustavose@gmail.com>
2025-10-02 13:06:06 -0300

    First test case in playwright

--- testDemo/example.spec.ts
+++ testDemo/example.spec.ts
@@ -1,18 +1,8 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, chromium } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link',{tag:"@first"}, async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+test('Sign in with Google Button is visible', async ({page}) => {
+  await page.goto('https://practicesoftwaretesting.com/auth/login')
+  const loginButton = page.getByRole('button',{name : 'Sign in with Google'} )
+  await expect(loginButton).toBeVisible()
+  
+})
